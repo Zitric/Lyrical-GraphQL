0e55362730c952b9b96cb7681f68ff2c8cbd8756
Zitric <zitricwebdeveloper@gmail.com>
2019-06-30 15:23:42 +0100

    first commit

---
+++ client/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class SongList extends React.Component {
+    render() {
+        return (
+        <div>
+            Song list
+        </div>
+        )
+    }   
+}
+
+export default SongList;
